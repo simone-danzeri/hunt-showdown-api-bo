f3b95b0d92e600ba2b8c516b9f93c2756853c477
simone-danzeri <s.danzeri@icloud.com>
2024-07-26 17:21:46 +0200

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-4 ms-bg-primary ms-login-mt">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card ms-card-border">
+                <div class="card-header ms-bg-primary ms-font-white text-center pb-5 fs-4">{{ __('Register') }}</div>
+
+                <div class="card-body ms-bg-primary ms-font-white">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="mb-4 row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6 pb-3">
+                                <input id="name" type="text" class="form-control ms-bg-secondary @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="mb-4 row pb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control ms-bg-secondary @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="mb-4 row pb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control ms-bg-secondary @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="mb-4 row pb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control ms-bg-secondary" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="mb-4 row mb-0">
+                            <div class="col-md-6 offset-md-4 pb-3">
+                                <button type="submit" class="btn btn-primary ms-btn">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+<div class="container ms-full-page">
+    <div class="main-title ms-title-location">
+        <h1 class="ms-font-white text-center">
+            Welcome to the unofficial Hunt Showdown Api
+        </h1>
+    </div>
+</div>
+@endsection
