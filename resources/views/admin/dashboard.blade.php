7436db5c811ac43e80473bafbc06d6ea734df912
simone-danzeri <s.danzeri@icloud.com>
2024-07-26 17:56:32 +0200

    feat: added dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.admin')
+@section('content')
+    <h1 class="ms-font-white">I'm the dashboard</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,15 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+// My routes
+Route::middleware(['auth', 'verified'])
+->name('admin.')
+->prefix('admin')
+->group(function() {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
