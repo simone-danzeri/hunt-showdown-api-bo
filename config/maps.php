0c71501a68a8f6484888800ee5ebe888278c4d5c
simone-danzeri <s.danzeri@icloud.com>
2024-08-25 16:42:11 +0200

    feat: added config file for maps

---
+++ config/maps.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    // Stillwater Bayou
+    [
+        "map_name" => "Stillwater Bayou",
+        "date_of_release" => "22-02-2018",
+        "summary" => "Stillwater Bayou is one of the four Maps in Hunt Showdown 1896 and is the first map located in Louisiana.",
+        "number_of_compouds" => 16
+
+    ],
+    // Lawson Delta
+    [
+        "map_name" => "Lawson Delta",
+        "date_of_release" => "22-02-2018",
+        "summary" => "Lawson Delta is one of the four Maps in Hunt Showdown 1896 and is the second map located in Louisiana.",
+        "number_of_compouds" => 16
+    ],
+    // DeSalle
+    [
+        "map_name" => "DeSalle",
+        "date_of_release" => "20-07-2021",
+        "summary" => "DeSalle is one of the four Maps in Hunt Showdown 1896 and is the third map located in Louisiana.",
+        "number_of_compouds" => 16
+    ],
+    // Mammon's Gluch
+    [
+        "map_name" => "Mammon's Gluch",
+        "date_of_release" => "15-08-2024",
+        "summary" => "DeSalle is one of the four Maps in Hunt Showdown 1896 and is the first map located in Colorado.",
+        "number_of_compouds" => 16
+    ]
+];
