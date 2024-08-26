e581985dec4adfdf37961d4accf5fdf4ef9c3210
simone-danzeri <s.danzeri@icloud.com>
2024-08-26 09:56:18 +0200

    feat: added config for game modes

---
+++ config/game_modes.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    [
+        "name" => "Bounty Hunt",
+        "summary" => "This is the classic game mode where players and their partners spawn in a random location, and the hunt begins. All player have to hunt the same boss and the quicker way of finndinng it is by searching three clues hidden around the map. When the third clue is found, the monster's location will be marked on the team's map that found them all. When the target monster is dead, players must banish its soul back to hell. During the Banishing, a howling storm will form around that team's location and a burning effect will mark them on the map to other players. When the banishing is complete, two bounty tokens will appear in place of the monster's corpse. Collect them - each Hunter can only collect one - and then the team has to prepare for the showdown, as other surviving players will flock to the location in hopes of stealing the prize. At this point the team can either head directly to the extraction point in order to exit the map and collect their bounty or risk going after the second target monster. After banishing, the Bounty Tokens continue to act as a spirit anchor. This anchor manifests itself in the form of flashes of lightning visible in Dark Sight. This means the location of a Hunter carrying a Bounty Token is visible to other Hunters. In order to cash in Bounty Tokens for cash, the team must exit the map via an Extraction Point while avoiding ambushes from other Hunters."
+    ],
+    [
+        "name" => "Soul Survivor",
+        "summary" => "A free for all mode where players spawn in the map without any equipment but a knife and a medipack and have to find and close 4 rifts in order to become the soul survivor. Once the rift are closed, the player has to survive for 240 seconds in order to extract with his hunter and all his found equipment but it's up to the other player to kill him and became the new soul survivor."
+    ]
+];
