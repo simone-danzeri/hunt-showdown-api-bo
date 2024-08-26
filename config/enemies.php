b8f6bdedbadf8794b09fee8486715224f8b88486
simone-danzeri <s.danzeri@icloud.com>
2024-08-26 15:35:15 +0200

    feat: added enemies config

---
+++ config/enemies.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    [
+        "enemy_name" => "Grunt",
+        "summary" => "Basically a zombie whose sole purpose is coming after you. He can wield torches, cleavers or other weapons."
+    ],
+    [
+        "enemy_name" => "Gun Grunt",
+        "summary" => "A zombie wielding an handgun that goes off if it hits your hunter with it."
+    ],
+    [
+        "enemy_name" => "Lantern Grunt",
+        "summary" => "A zombie wielding an lantern that explode if it hits your hunter with it setting the surroundings on fire."
+    ],
+    [
+        "enemy_name" => "Armored",
+        "summary" => "The Armored is a common monster that a Hunter will encounter while out in the swamps. They can easily be defined from other zombies by the chitinous plates growing out from their body. This armor provides them a strong resistance against most, if not all, kinds of kinetic weaponry. Armored make a significant amount of noise when noticing a player, and it is no quiet task taking one down. This, paired with the fact that that armored are most commonly found in compounds where hunters tend to skirmish, means they are detrimental to any kind of stealthy approach."
+    ],
+    [
+        "enemy_name" => "Concertina Armored",
+        "summary" => "An armored with concertina wire all around him."
+    ],
+    [
+        "enemy_name" => "Hellhound",
+        "summary" => "An Italian Mastiff which hunt in packs of 2 or 4. His bite can take out even the most experienced hunters."
+    ],
+    [
+        "enemy_name" => "Hive",
+        "summary" => "Hives are monsters easily triggered that sends poisonous bees growing insied of them to chase you down."
+    ],
+    [
+        "enemy_name" => "Waterdevils",
+        "summary" => "Aquatic monsters that guards the waters of Bayou and Colorado."
+    ],
+    [
+        "enemy_name" => "Immolators",
+        "summary" => "The Immolator seems to be the most tormented of them all. The smallest movement sends him into a rage, attacking chaotically, and when shot he burns even hotter and charges like an angry bull. He is, of course, extinguished in water (and can be fatally consumed by his own internal flames), but otherwise, represents the most intelligent example I have seen."
+    ],
+    [
+        "enemy_name" => "Meathead",
+        "summary" => "A huge beast who chase down poisoned hunter thanks to the leaches it spawns."
+    ],
+];
