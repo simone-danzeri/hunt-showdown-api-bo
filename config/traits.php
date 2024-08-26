24da1c6750e02ba375be2877b1d177c8eac4a25f
simone-danzeri <s.danzeri@icloud.com>
2024-08-26 11:22:10 +0200

    feat: start of traits config (up to physician)

---
+++ config/traits.php
@@ -0,0 +1,219 @@
+<?php
+return [
+    [
+        "trait_name" => "Adrenaline",
+        "trait_img" => "traits/adrenaline.webp",
+        "points" => 1,
+        "summary" => "Instantly start regenerating Stamina while your Health is critically low. Useful after just being revived."
+    ],
+    [
+        "trait_name" => "Ambidextrous",
+        "trait_img" => "traits/ambidextrous.webp",
+        "points" => 3,
+        "summary" => "Quicker reloading of matched pairs, and custom clip reloads for semi-auto pistol sets."
+    ],
+    [
+        "trait_name" => "Assailant",
+        "trait_img" => "traits/assailant.webp",
+        "points" => 1,
+        "summary" => "Increases melee effectiveness of Throwing Knives and Throwing Axes. Melee weapons found in the World can be thrown."
+    ],
+    [
+        "trait_name" => "Beastface",
+        "trait_img" => "traits/beastface.webp",
+        "points" => 4,
+        "summary" => "Reduced reaction range of animals (doesn't affect Monsters like Hellhounds)."
+    ],
+    [
+        "trait_name" => "Blade Seer",
+        "trait_img" => "traits/blade_seer.webp",
+        "points" => 1,
+        "summary" => "Bolts, arrows, throwing axes, throwing knives and spears are highlighted in Dark Sight for better visibility. Also applies to Beetle view (25m)."
+    ],
+    [
+        "trait_name" => "Bloodless",
+        "trait_img" => "traits/bloodless.webp",
+        "points" => 3,
+        "summary" => "Bleeding will not escalate from light to medium or intense bleeding."
+    ],
+    [
+        "trait_name" => "Bolt Thrower",
+        "trait_img" => "traits/bolt_thrower.webp",
+        "points" => 3,
+        "summary" => "Reduced reload time for Crossbows."
+    ],
+    [
+        "trait_name" => "Bulletgrubber",
+        "trait_img" => "traits/bulletgrubber.webp",
+        "points" => 4,
+        "summary" => "Recover the unfired round when performing partial reloads."
+    ],
+    [
+        "trait_name" => "Bulwark",
+        "trait_img" => "traits/bulwark.webp",
+        "points" => 2,
+        "summary" => "Reduce the damage from explosions and Bomb Lance harpoon attacks by 50%. Vastly reduces the duration of being fully blinded by a Flash Bomb."
+    ],
+    [
+        "trait_name" => "Conduit",
+        "trait_img" => "traits/conduit.webp",
+        "points" => 5,
+        "summary" => "Get a Health and Stamina boost when your team picks up a Clue or closes a Rift."
+    ],
+    [
+        "trait_name" => "Dauntless",
+        "trait_img" => "traits/dauntless.webp",
+        "points" => 1,
+        "summary" => "Thrown Explosives can be defused when interacting with them."
+    ],
+    [
+        "trait_name" => "Decoy Supply",
+        "trait_img" => "traits/decoy_supply.webp",
+        "points" => 1,
+        "summary" => "Restock all types of Decoys from ammo crates."
+    ],
+    [
+        "trait_name" => "Determination",
+        "trait_img" => "traits/determination.webp",
+        "points" => 4,
+        "summary" => "Stamina recovery starts sooner."
+    ],
+    [
+        "trait_name" => "Doctor",
+        "trait_img" => "traits/doctor.webp",
+        "points" => 9,
+        "summary" => "Doubles the amount of Health restored by First Aid Kits."
+    ],
+    [
+        "trait_name" => "Fanning",
+        "trait_img" => "traits/fanning.webp",
+        "points" => 8,
+        "summary" => "Faster rate of fire when using 1-handed single-action pistols."
+    ],
+    [
+        "trait_name" => "Frontiersman",
+        "trait_img" => "traits/frontiersman.webp",
+        "points" => 7,
+        "summary" => "Carried Tools can be used one extra time."
+    ],
+    [
+        "trait_name" => "Gator Legs",
+        "trait_img" => "traits/gator_legs.webp",
+        "points" => 3,
+        "summary" => "Walk and sprint faster in deep water. Also make less noise while crouched in water."
+    ],
+    [
+        "trait_name" => "Ghoul",
+        "trait_img" => "traits/ghoul.webp",
+        "points" => 3,
+        "summary" => "Killing Monsters restores a small amount of Health."
+    ],
+    [
+        "trait_name" => "Greyhound",
+        "trait_img" => "traits/greyhound.webp",
+        "points" => 5,
+        "summary" => "Sprint at full speed for a longer duration."
+    ],
+    [
+        "trait_name" => "Hornskin",
+        "trait_img" => "traits/hornskin.webp",
+        "points" => 3,
+        "summary" => "Reduce damage taken from blunt melee by 25%."
+    ],
+    [
+        "trait_name" => "Hundred Hands",
+        "trait_img" => "traits/hundred_hands.webp",
+        "points" => 2,
+        "summary" => "Increases the damage of a Bow and reduces sway while at full draw. Allows use of arrows in melee attacks."
+    ],
+    [
+        "trait_name" => "Iron Eye",
+        "trait_img" => "traits/iron_eye.webp",
+        "points" => 3,
+        "summary" => "Remain in iron sights between shots when firing bolt-action, lever-action, and pump-action weapons."
+    ],
+    [
+        "trait_name" => "Kiteskin",
+        "trait_img" => "traits/kiteskin.webp",
+        "points" => 1,
+        "summary" => "Reduce damage from falling by 50%."
+    ],
+    [
+        "trait_name" => "Levering",
+        "trait_img" => "traits/levering.webp",
+        "points" => 4,
+        "summary" => "Faster rate of fire from the hip when using lever-action weapons."
+    ],
+    [
+        "trait_name" => "Lightfoot",
+        "trait_img" => "traits/lightfoot.webp",
+        "points" => 5,
+        "summary" => "Vault, fall, and climb ladders silently. Also reduces the sound of walking through noise traps."
+    ],
+    [
+        "trait_name" => "Magpie",
+        "trait_img" => "traits/magpie.webp",
+        "points" => 1,
+        "summary" => "Receive three short effects when picking up a Bounty Token: Antidote, Stamina, and Regeneration. SOLO: Dark Sight Boost capacity in all Boost sources are doubled."
+    ],
+    [
+        "trait_name" => "Martialist",
+        "trait_img" => "traits/martialist.webp",
+        "points" => 2,
+        "summary" => "Allows a Katana to be sheathed, which enables a first strike attack that does extra damage."
+    ],
+    [
+        "trait_name" => "Mithridatist",
+        "trait_img" => "traits/mithridatist.webp",
+        "points" => 3,
+        "summary" => "Drastically reduces the time needed to recover from poisoning."
+    ],
+    [
+        "trait_name" => "Necromancer",
+        "trait_img" => "traits/necromancer.webp",
+        "points" => 4,
+        "summary" => "Burn trait. Using Dark Sight, revive a downed partner from a distance (25m). SOLO: You can revive your downed Hunter with a restoration effect."
+    ],
+    [
+        "trait_name" => "Packmule",
+        "trait_img" => "traits/packmule.webp",
+        "points" => 4,
+        "summary" => "Receive an additional Tool or Consumable when looting players or opening item boxes."
+    ],
+    [
+        "trait_name" => "Physician",
+        "trait_img" => "traits/physician.webp",
+        "points" => 5,
+        "summary" => "Reduce the time needed to bandage."
+    ],
+    [
+        "trait_name" => "",
+        "trait_img" => "traits/",
+        "points" => 0,
+        "summary" => ""
+    ],
+    [
+        "trait_name" => "",
+        "trait_img" => "traits/",
+        "points" => 0,
+        "summary" => ""
+    ],
+    [
+        "trait_name" => "",
+        "trait_img" => "traits/",
+        "points" => 0,
+        "summary" => ""
+    ],
+    [
+        "trait_name" => "",
+        "trait_img" => "traits/",
+        "points" => 0,
+        "summary" => ""
+    ],
+    [
+        "trait_name" => "",
+        "trait_img" => "traits/",
+        "points" => 0,
+        "summary" => ""
+    ],
+];
