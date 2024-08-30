0befedd51da9af81a31186844f895e301a8c9889
simone-danzeri <s.danzeri@icloud.com>
2024-08-30 16:25:36 +0200

    feat: route for weapon

---
+++ resources/views/admin/weapons/index.blade.php
@@ -0,0 +1,4 @@
+@extends('admin.dashboard')
+@section('content')
+    <h1>Hi I'm the index</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\WeaponController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +30,7 @@ Route::middleware(['auth', 'verified'])
 ->prefix('admin')
 ->group(function() {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/weapons', WeaponController::class);
 });
 
 
